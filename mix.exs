2b17b28a9c0120344e7a61b8249b4aaaa58a6ce4
Bradley Smith <bradleydsmith@gmail.com>
2015-05-31 08:22:28 -0600

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule DockerHostHeartbeat.Mixfile do
 
   def project do
     [app: :docker_host_heartbeat,
-     version: "0.2.0",
+     version: "0.3.0",
      elixir: "~> 1.0",
      deps: deps]
   end
